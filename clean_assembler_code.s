e3acd0d5b26b2b86aac8b370f09916ea9e9ae88a
rmgaliullin <45234065+RuslanGaliullin@users.noreply.github.com>
2022-09-14 22:58:56 +0300

    Assembly hello world is done

--- clean_assembler_code.s
+++ clean_assembler_code.s
@@ -1,7 +1,19 @@
-.intel_syntax noprefix
-.text
-.global _start
+section .data
+	hello:     db 'Hello world!',10
+	len:  equ $-hello          
+	                                 
+
+section .text
+	global _start
+
 _start:
-    mov rax, 60
-    mov radi, 0
-    syscall
+	mov eax,4            
+	mov ebx,1            
+	mov ecx,hello        
+	mov edx,helloLen     
+	                    
+	int 80h              
+
+	mov eax,1           
+	mov ebx,0    
+	int 80h
