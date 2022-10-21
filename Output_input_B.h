334033733bf616663c8b4c7abba4c8e0d2506cfd
rmgaliullin <45234065+RuslanGaliullin@users.noreply.github.com>
2022-10-21 23:19:41 +0300

    Header for the exe

---
+++ Output_input_B.h
@@ -0,0 +1,9 @@
+#ifndef CAAOS__OUTPUT_INPUT_B_H_
+#define CAAOS__OUTPUT_INPUT_B_H_
+#include <stdio.h>
+void BuildBArray(int B[], int A[], int size);
+
+int ReadFromFile(FILE *fin, int A[]);
+void Output(FILE *ofst, int B[], int size);
+void InputArrayFromConsole(int A[], int size);
+#endif
