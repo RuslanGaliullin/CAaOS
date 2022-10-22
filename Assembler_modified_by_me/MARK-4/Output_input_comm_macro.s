dbd58fb771b7a5a1da023f56082fb02c0907d2ba
rmgaliullin <45234065+RuslanGaliullin@users.noreply.github.com>
2022-10-22 11:38:19 +0300

    Create Output_input_comm_macro.s
    Написаны комментарии, удалены макросы

---
+++ Assembler_modified_by_me/MARK-4/Output_input_comm_macro.s
@@ -0,0 +1,211 @@
+        .file   "Output_input_B.c"
+        .intel_syntax noprefix
+        .text
+        .globl  BuildBArray
+        .type   BuildBArray, @function
+BuildBArray:
+        endbr64
+        push    rbp
+        mov     rbp, rsp
+        mov     QWORD PTR -24[rbp], rdi         # QWORD PTR -24[rbp] - переменная int B[]
+        mov     QWORD PTR -32[rbp], rsi         # QWORD PTR -32[rbp] - переменная int A[]
+        mov     DWORD PTR -36[rbp], edx         # QWORD PTR -36[rbp] - переменная int size
+        mov     DWORD PTR -4[rbp], 1            # DWORD PTR -4[rbp] - переменная int i = 1
+        jmp     .L2
+.L3:
+        mov     eax, DWORD PTR -4[rbp]
+        cdqe
+        lea     rdx, 0[0+rax*4]
+        mov     rax, QWORD PTR -32[rbp]
+        add     rax, rdx
+        mov     edx, DWORD PTR -4[rbp]
+        sub     edx, 1
+        mov     ecx, edx
+        shr     ecx, 31
+        add     edx, ecx
+        sar     edx
+        movsx   rdx, edx
+        lea     rcx, 0[0+rdx*4]
+        mov     rdx, QWORD PTR -24[rbp]
+        add     rdx, rcx
+        mov     eax, DWORD PTR [rax]
+        mov     DWORD PTR [rdx], eax
+        add     DWORD PTR -4[rbp], 2
+.L2:
+        mov     eax, DWORD PTR -4[rbp]
+        cmp     eax, DWORD PTR -36[rbp]
+        jl      .L3
+        mov     DWORD PTR -8[rbp], 0            # DWORD PTR -8[rbp] - переменная int i = 0
+        jmp     .L4
+.L5:
+        mov     eax, DWORD PTR -8[rbp]
+        cdqe
+        lea     rdx, 0[0+rax*4]
+        mov     rax, QWORD PTR -32[rbp]
+        lea     rcx, [rdx+rax]
+        mov     eax, DWORD PTR -36[rbp]
+        mov     edx, eax
+        shr     edx, 31
+        add     eax, edx
+        sar     eax
+        mov     esi, eax
+        mov     eax, DWORD PTR -8[rbp]
+        mov     edx, eax
+        shr     edx, 31
+        add     eax, edx
+        sar     eax
+        add     eax, esi
+        cdqe
+        lea     rdx, 0[0+rax*4]
+        mov     rax, QWORD PTR -24[rbp]
+        add     rdx, rax
+        mov     eax, DWORD PTR [rcx]
+        mov     DWORD PTR [rdx], eax
+        add     DWORD PTR -8[rbp], 2
+.L4:
+        mov     eax, DWORD PTR -8[rbp]
+        cmp     eax, DWORD PTR -36[rbp]
+        jl      .L5
+        nop
+        nop
+        pop     rbp
+        ret
+        .size   BuildBArray, .-BuildBArray
+        .section        .rodata
+.LC0:
+        .string "%d "
+        .text
+        .globl  Output
+        .type   Output, @function
+Output:
+        endbr64
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 48
+        mov     QWORD PTR -24[rbp], rdi             # QWORD PTR -24[rbp] - переменная FILE *ofst
+        mov     QWORD PTR -32[rbp], rsi             # QWORD PTR -32[rbp] - переменная int B[]
+        mov     DWORD PTR -36[rbp], edx             # DWORD PTR -36[rbp] - переменная int size
+        mov     DWORD PTR -4[rbp], 0                # DWORD PTR -4[rbp] - переменная int i = 0
+        jmp     .L7
+.L8:
+        mov     eax, DWORD PTR -4[rbp]
+        cdqe
+        lea     rdx, 0[0+rax*4]
+        mov     rax, QWORD PTR -32[rbp]
+        add     rax, rdx
+        mov     edx, DWORD PTR [rax]
+        mov     rax, QWORD PTR -24[rbp]
+        lea     rcx, .LC0[rip]
+        mov     rsi, rcx
+        mov     rdi, rax
+        mov     eax, 0
+        call    fprintf@PLT
+        add     DWORD PTR -4[rbp], 1
+.L7:
+        mov     eax, DWORD PTR -4[rbp]
+        cmp     eax, DWORD PTR -36[rbp]
+        jl      .L8
+        mov     rax, QWORD PTR -24[rbp]
+        mov     rsi, rax
+        mov     edi, 10
+        call    fputc@PLT
+        nop
+        leave
+        ret
+        .size   Output, .-Output
+        .section        .rodata
+.LC1:
+        .string "%d"
+        .text
+        .globl  ReadFromFile
+        .type   ReadFromFile, @function
+ReadFromFile:
+        endbr64
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 32
+        mov     QWORD PTR -24[rbp], rdi                 # QWORD PTR -24[rbp] - переменная FILE *fin
+        mov     QWORD PTR -32[rbp], rsi                 # QWORD PTR -32[rbp] - переменная int A[]
+        lea     rdx, -8[rbp]                            # DWORD PTR -8[rbp] - переменная int size
+        mov     rax, QWORD PTR -24[rbp]
+        lea     rcx, .LC1[rip]
+        mov     rsi, rcx
+        mov     rdi, rax
+        mov     eax, 0
+        call    __isoc99_fscanf@PLT
+        cmp     eax, -1
+        jne     .L10
+        mov     eax, -1
+        jmp     .L17
+.L10:
+        mov     eax, DWORD PTR -8[rbp]
+        test    eax, eax
+        jle     .L12
+        mov     eax, DWORD PTR -8[rbp]
+        cmp     eax, 10000000
+        jle     .L13
+.L12:
+        mov     eax, -2
+        jmp     .L17
+.L13:
+        mov     DWORD PTR -4[rbp], 0                    # DWORD PTR -4[rbp] - переменная int i = 0
+        jmp     .L14
+.L16:
+        mov     eax, DWORD PTR -4[rbp]
+        cdqe
+        lea     rdx, 0[0+rax*4]
+        mov     rax, QWORD PTR -32[rbp]
+        add     rdx, rax
+        mov     rax, QWORD PTR -24[rbp]
+        lea     rcx, .LC1[rip]
+        mov     rsi, rcx
+        mov     rdi, rax
+        mov     eax, 0
+        call    __isoc99_fscanf@PLT
+        cmp     eax, -1
+        jne     .L15
+        mov     eax, -1
+        jmp     .L17
+.L15:
+        add     DWORD PTR -4[rbp], 1
+.L14:
+        mov     eax, DWORD PTR -8[rbp]
+        cmp     DWORD PTR -4[rbp], eax
+        jl      .L16
+        mov     eax, DWORD PTR -8[rbp]
+.L17:
+        leave
+        ret
+        .size   ReadFromFile, .-ReadFromFile
+        .globl  InputArrayFromConsole
+        .type   InputArrayFromConsole, @function
+InputArrayFromConsole:
+        endbr64
+        push    rbp
+        mov     rbp, rsp
+        sub     rsp, 32
+        mov     QWORD PTR -24[rbp], rdi                 # QWORD PTR -24[rbp] - переменная int A[]      
+        mov     DWORD PTR -28[rbp], esi                 # DWORD PTR -28[rbp] - переменная int size
+        mov     DWORD PTR -4[rbp], 0                    # DWORD PTR -4[rbp] - переменная int i = 0
+        jmp     .L19
+.L20:
+        mov     eax, DWORD PTR -4[rbp]
+        cdqe
+        lea     rdx, 0[0+rax*4]
+        mov     rax, QWORD PTR -24[rbp]
+        add     rax, rdx
+        mov     rsi, rax
+        lea     rax, .LC1[rip]
+        mov     rdi, rax
+        mov     eax, 0
+        call    __isoc99_scanf@PLT
+        add     DWORD PTR -4[rbp], 1
+.L19:
+        mov     eax, DWORD PTR -4[rbp]
+        cmp     eax, DWORD PTR -28[rbp]
+        jl      .L20
+        nop
+        nop
+        leave
+        ret
+        .size   InputArrayFromConsole, .-InputArrayFromConsole
