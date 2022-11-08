06e9fb3bd20413ddca58541818613abfa25e5c6f
rmgaliullin <rmgaliullin@edu.hse.ru>
2022-11-08 18:54:41 +0000

    remove previous files

--- Output_input_B.h
+++
@@ -1,9 +0,0 @@
-#ifndef CAAOS__OUTPUT_INPUT_B_H_
-#define CAAOS__OUTPUT_INPUT_B_H_
-#include <stdio.h>
-void BuildBArray(int B[], int A[], int size);
-
-int ReadFromFile(FILE *fin, int A[]);
-void Output(FILE *ofst, int B[], int size);
-void InputArrayFromConsole(int A[], int size);
-#endif//CAAOS__OUTPUT_INPUT_B_H_
